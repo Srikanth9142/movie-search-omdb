7b284f4e3efa34376328cefae5317213d5d5b464
srikanth9142 <srikanthtumpudi1999@gmail.com>
2020-07-22 11:59:30 +0530

    Updates

---
+++ precache-manifest.710b502f87bd5b0746c6e1076c35a8ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39606f7cfcb425b8bdfd540c44c3d74",
+    "url": "/movie-search-omdb/index.html"
+  },
+  {
+    "revision": "13f0d9228a3725631b12",
+    "url": "/movie-search-omdb/static/css/main.375b9202.chunk.css"
+  },
+  {
+    "revision": "38d69d38430e7cc0901d",
+    "url": "/movie-search-omdb/static/js/2.f243da46.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie-search-omdb/static/js/2.f243da46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f0d9228a3725631b12",
+    "url": "/movie-search-omdb/static/js/main.9e1be7f5.chunk.js"
+  },
+  {
+    "revision": "4344ea8f39c5548417c0",
+    "url": "/movie-search-omdb/static/js/runtime-main.826c30b7.js"
+  }
+]);
